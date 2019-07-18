04a48b509bd0e867c7af2336bc0fa1004eec62c7
白鳥雅人 <whitesplash0703@icloud.com>
2019-07-18 19:44:34 +0900

    Lesson5課題
    PinBallゲームの作成と、得点の表示をしました。

---
+++ Assets/BallController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+    private float visiblePosZ = -6.5f;
+    private GameObject gameoverText;
+	GameObject director;
+
+	// Use this for initialization
+	void Start () {
+		this.director = GameObject.Find ("GameDirector");
+        this.gameoverText = GameObject.Find("GameOverText");
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "SmallStarTag") {
+			this.director.GetComponent<GameDirector>().GetSmallStar();
+			Debug.Log ("SmallStar");
+		} else if (other.gameObject.tag == "LargeStarTag") {
+			this.director.GetComponent<GameDirector>().GetLargeStar();
+			Debug.Log ("LargeStar");
+		} else if (other.gameObject.tag == "SmallCloudTag") {
+			this.director.GetComponent<GameDirector>().GetSmallCloud();
+			Debug.Log ("SmallCloud");
+		} else if (other.gameObject.tag == "LargeCloudTag") {
+			this.director.GetComponent<GameDirector>().GetLargeCloud();
+			Debug.Log ("LargeCloud");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.z < this.visiblePosZ)
+        {
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+	}
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+	private GameObject pointText;
+	public int point = 0;
+
+	public void GetSmallStar() {
+		this.point += 1;
+	}
+
+	public void GetLargeStar() {
+		this.point += 10;
+	}
+
+	public void GetSmallCloud() {
+		this.point += 50;
+	}
+
+	public void GetLargeCloud() {
+		this.point += 100;
+	}
+	// Use this for initialization
+	void Start () {
+		this.pointText = GameObject.Find ("PointText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.pointText.GetComponent<Text>().text =
+			this.point.ToString() + " point";
+	}
+}
